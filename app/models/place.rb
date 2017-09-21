c7a65a7f304b8fd6b5712cb154e4613844bb8462
“Veronica <vmatovich@gmail.com>
2017-09-21 19:18:32 +0000

    added name length, address and description validation

--- app/models/place.rb
+++ app/models/place.rb
@@ -6,6 +6,13 @@ geocoded_by :address
 
 after_validation :geocode 
 
-validates :name, presence: true
+validates :name, presence: true 
+
+validates :name, length: { minimum: 3 }
+
+validates :address, presence: true
+
+validates :description, presence: true
+
 
 end
