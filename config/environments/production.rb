11dcd32bf4ee5ef34edbbd561869e837f285ebd7
“Veronica <vmatovich@gmail.com>
2017-10-05 15:20:10 +0000

    configured SendGrid via Heroku in Ruby production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,17 @@
 Rails.application.configure do
+
+
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
+  
+  }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -83,4 +96,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
 end
